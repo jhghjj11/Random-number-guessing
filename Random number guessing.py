ee85d5b37e6f56da8a646a626b78783ef9089ee9
jhghjj11 <57930789+jhghjj11@users.noreply.github.com>
2020-08-02 00:13:11 +0800

    Add files via upload

--- Random number guessing.py
+++ Random number guessing.py
@@ -1,13 +1,19 @@
 import random
-r = random.randint(1, 100)
+r = random.randint(0, 100)
+
+count = 0
 
 while True :
     answer = input('Type a integer :')
     answer = int(answer)
     if answer == r :
         print('You got it!')
+        print('Total tried:', count,'times')
         break
     elif answer > r :
         print('Try a smaller one')
+        count = count + 1
     else :
         print('Try a bigger one')
+        count = count + 1
+    print('You have tried', count,'times')
